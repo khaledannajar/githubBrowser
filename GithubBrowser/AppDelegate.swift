9f1d68225c8e17d9206303b4322b5cebd178cab6
Khaled Annajar <khaledannajar@gmail.com>
2019-01-11 09:48:37 +0300

    initial commit for project that builds and runs

---
+++ GithubBrowser/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GithubBrowser
+//
+//  Created by khaledannajar on 1/11/19.
+//  Copyright © 2019 khaledannajar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ GithubBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubBrowser
+//
+//  Created by khaledannajar on 1/11/19.
+//  Copyright © 2019 khaledannajar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
